0c8bfa6968a4f34898e776d8a0d3fef126a3192d
pfcDorn <dorn@prefrontalcortex.de>
2024-05-21 17:25:19 +0200

    added virtualspeaker class

---
+++ jp.keijiro.klak.ndi/Runtime/Component/VirtualAudio/VirtualSpeaker.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+namespace Klak.Ndi.Audio
+{
+	internal class VirtualSpeakers
+	{
+		public AudioSource speakerAudio;
+		public Vector3 relativePosition;
+		private AudioSourceBridge _audioSourceBridge;
+
+		public void CreateGameObjectWithAudioSource(Transform parent, Vector3 localPosition)
+		{
+			var go = new GameObject();
+			go.transform.parent = parent;
+			go.transform.localPosition = localPosition;
+			go.name = "VirtualSpeaker";
+			go.SetActive(false);
+			go.hideFlags = HideFlags.NotEditable | HideFlags.DontSave;
+			speakerAudio = go.AddComponent<AudioSource>();
+			speakerAudio.loop = true;
+			speakerAudio.rolloffMode = AudioRolloffMode.Custom;
+			speakerAudio.SetCustomCurve( AudioSourceCurveType.CustomRolloff, AnimationCurve.Linear(0, 1, 1, 1) );
+			speakerAudio.maxDistance = 500;
+			speakerAudio.spatialBlend = 1f;
+
+			if (!string.IsNullOrEmpty(AudioSettings.GetSpatializerPluginName()))
+			{
+				speakerAudio.spatialize = true;
+				speakerAudio.spatializePostEffects = true;
+			}
+			
+			go.SetActive(true);
+			
+			speakerAudio.Play();
+		}
+		
+		public void CreateAudioSourceBridge(NdiReceiver handle, int channelNo, int maxChannels, int systemChannels)
+		{
+			if (speakerAudio == null)
+				return;
+
+			if (_audioSourceBridge != null)
+				return;
+
+			speakerAudio.gameObject.name = $"VirtualSpeaker {channelNo}";
+			_audioSourceBridge = speakerAudio.gameObject.AddComponent<AudioSourceBridge>();
+			_audioSourceBridge._handler = handle;
+			_audioSourceBridge.Init(true, systemChannels, channelNo, maxChannels, !string.IsNullOrEmpty(AudioSettings.GetSpatializerPluginName()));
+		}
+		
+		public void DestroyAudioSourceBridge()
+		{
+			if (_audioSourceBridge == null)
+				return;
+
+			_audioSourceBridge._handler = null;
+
+			if(_audioSourceBridge._isDestroyed == false)
+				GameObject.DestroyImmediate(_audioSourceBridge);
+
+			_audioSourceBridge = null;
+		}
+	}
+}
