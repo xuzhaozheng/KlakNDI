c78736bdbe13908ff495816d7e1f5553e81f8e28
pfcDorn <dorn@prefrontalcortex.de>
2024-06-13 14:42:49 +0200

    added resolution dropdown for debug sender project

---
+++ NDIDebugSender/Assets/Scripts/RenderTextureSetup.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Klak.Ndi;
+using TMPro;
+using UnityEngine;
+
+public class RenderTextureSetup : MonoBehaviour
+{
+    [SerializeField] private NdiSender _ndiSender;
+    [SerializeField] private Camera _camera;
+    [SerializeField] private TMP_Dropdown _presetDropdown;
+    
+    [Serializable]
+    public struct ResolutionPreset
+    {
+        public int width;
+        public int height;
+    }
+
+    [SerializeField] private ResolutionPreset[] preset;
+    private RenderTexture _renderTexture;
+    
+    public void SelectPreset(int index)
+    {
+        if (index < 0 || index >= preset.Length)
+        {
+            Debug.LogError("Invalid index");
+            return;
+        }
+        
+        ChangeResolution(preset[index].width, preset[index].height);
+    }
+    
+    public void ChangeResolution(int width, int height)
+    {
+        _ndiSender.enabled = false;
+
+        if (_renderTexture != null)
+        {
+            _renderTexture.Release();
+            Destroy(_renderTexture);
+        }
+        _renderTexture = new RenderTexture(width,height,24,RenderTextureFormat.ARGB32);
+        
+        _ndiSender.sourceTexture = _renderTexture;
+        _camera.targetTexture = _renderTexture;
+        _ndiSender.enabled = true;
+    }
+
+    private void CreatePresetDropDown()
+    {
+        _presetDropdown.ClearOptions();
+        List<string> options = new List<string>();
+        foreach (var resolutionPreset in preset)
+        {
+            options.Add($"{resolutionPreset.width}x{resolutionPreset.height}");
+        }
+        _presetDropdown.AddOptions(options);
+        _presetDropdown.SetValueWithoutNotify(0);
+    }
+
+    private void Awake()
+    {
+        CreatePresetDropDown();
+        _presetDropdown.onValueChanged.AddListener(SelectPreset);
+    }
+}
